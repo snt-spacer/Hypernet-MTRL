c4d96f4b0bf16362b0ba6c6f185cfb676b9444bf
ricard-inho <ricardmarsalicastan@gmail.com>
2025-04-11 09:56:24 +0200

    train bash file rsl

---
+++ scripts/reinforcement_learning/rsl_rl/train.sh
@@ -0,0 +1,16 @@
+robots=(Jetbot Turtlebot Kingfisher Leatherback)
+tasks=(GoToPosition GoToPose GoThroughPositions GoThroughPoses RaceGates TrackVelocities)
+
+for robot in "${robots[@]}"
+do
+    for task in "${tasks[@]}"
+    do
+        for seed in {1..5}
+        do
+            for run in {1..5}
+            do
+                ./isaaclab.sh -p scripts/reinforcement_learning/rsl_rl/train.py --task=Isaac-${robot}-${task}-Direct-v0 --run_num=${run} --seed=${seed} --headless
+            done
+        done
+    done
+done
