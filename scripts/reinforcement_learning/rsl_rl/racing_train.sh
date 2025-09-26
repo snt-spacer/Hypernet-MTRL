5af80548ca55b16f190a46a3d94dee6b1031c859
Ricard <ricardmarsalicastan@gmail.com>
2025-09-26 09:41:58 +0200

    readme for paper

--- scripts/reinforcement_learning/rsl_rl/racing_train.sh
+++ scripts/reinforcement_learning/rsl_rl/racing_train.sh
@@ -8,8 +8,8 @@ TASK="Isaac-RANS-Single-v0"
 ROBOT_NAME="Leatherback"
 TASK_NAME="RaceGates"
 ALGORITHM="ppo"
-TYPE_OF_TRAINING="padd"  # Options: "hyper" or "padd"
-FIXED_TRACK_ID=0 # 0 is custom track, -1 for random
+TYPE_OF_TRAINING="hyper"  # Options: "hyper" or "padd"
+FIXED_TRACK_ID=-1 # 0 is custom track, -1 for random
 CUSTOM_TRACK_ID=0 # 0 BCN, 1 JPN, 2 Monza, 10 points, 4 points, 11 alphapilot
 SAME_TRACK_FOR_ALL_ENVS="True"  # If True, all environments will use the same
 
